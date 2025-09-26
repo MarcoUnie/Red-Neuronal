cc7b3cfbb743710d6207697bc95b76e2e3aebf3e
mmageco777 <mmageco777@gmail.com>
2025-09-26 09:18:03 +0000

    main prueba

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import numpy as np
-print(1 / (1 + np.exp(-2)))
+from capa import Capa
+from mlp import MLP
+
+if __name__ == "__main__":
+    x = np.array([
+        [0.1, 0.2, 0.3],
+        [0.5, -0.2, 0.1],
+        [-0.3, 0.8, -0.5],
+        [0.0, 0.0, 0.0]
+    ])
+    red = MLP([
+        Capa(num_entradas=3, num_neuronas=4, activacion=1 / (1 + np.exp(-x))),
+        Capa(num_entradas=4, num_neuronas=3, activacion=1 / (1 + np.exp(-x))),
+        Capa(num_entradas=3, num_neuronas=1, activacion=np.maximum(0, x))
+    ])
+    y_pred = red.predecir(X)
+    print("Predicciones:\n", y_pred)
--- mlp.py
+++ mlp.py
@@ -4,11 +4,9 @@ from capa import Capa
 
 class MLP:
     def __init__(self, capas: List[Capa]):
-        """Inicializa el MLP con una lista de capas."""
         self.capas = capas
 
     def predecir(self, entradas: np.ndarray) -> np.ndarray:
-        """Realiza la propagación hacia adelante en todas las capas."""
         salida = entradas
         for capa in self.capas:
             salida = capa.forward(salida)
